a3cfafdd516198cd4d9848902ff6a66b34900829
joshua3258 <odebodejoshua18@gmail.com>
2023-11-01 16:04:49 -0700

    mutiply

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiples two numbers
+ * @argc: number of command lines arg
+ * @argv: the array that contains the command line arg
+ * Return: 0 when successful
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[i]) * atoi(argv[2]));
+	return (0);
+}
+
