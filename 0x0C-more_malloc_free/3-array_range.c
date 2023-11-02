1607b82229db3d146972779fa7f5c7593b15ed43
joshua3258 <odebodejoshua18@gmail.com>
2023-11-02 07:39:00 -0700

    maximux array

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - function that create an array of integers
+ * @min: min parameter
+ * @max: max parameter
+ * Return: int pointer to the allocated memory
+ */
+int *array_range(int min, int max)
+{
+	int i, l;
+	int *a;
+
+	if (min > max)
+		return (NULL);
+	l = max - min + 1;
+	a = malloc(sizeof(int) * l);
+	if (a == NULL)
+		return (NULL);
+	for (i = 0; i < l; i++)
+	{
+		a[i] = min;
+		min++;
+	}
+	return (a);
+}
