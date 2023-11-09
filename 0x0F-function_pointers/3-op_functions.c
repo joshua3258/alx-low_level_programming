e7f0229123500dcb13f4c2cadeaf64000a3472a6
joshua3258 <odebodejoshua18@gmail.com>
2023-11-08 16:16:02 -0800

    program of simple operation

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+#include "3-calc.h"
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+/**
+ * op_add - Returns the sum of two numbers
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtraction of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the subtraction of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the multiplication of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division of two numbers
+ * @a: first number
+ * b: second number
+ * Return: retrun division of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - reminder of two number
+ * @a: first number
+ * @b: second number
+ * Return: return the reminder of a and b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
