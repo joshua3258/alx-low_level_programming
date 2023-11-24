a8cf9591f626f5fc7c297d38d22d471ded4e230b
joshua3258 <odebodejoshua18@gmail.com>
2023-11-24 01:27:08 -0800

    print binary number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * _pow - func that calc (base ^ power)
+ * @base: base of theexponent
+ * @power: power of the exponent
+ * Return: value of (base ^ power)
+ */
+unsigned long int _pow(unsigned int base, unsigned int power)
+{
+	unsigned long int num;
+	unsigned int a;
+
+	num = 1;
+	for (a = 1; a <= power; a++)
+		num *= base;
+	return (num);
+}
+
+/**
+ * print_binary - function that print the binary representation of a number
+ * @n: number to print
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int divisor, check;
+	char flag;
+
+	flag = 0;
+	divisor = (unsigned long int)_pow(2, sizeof(unsigned long int) * 8 - 1);
+	while (divisor != 0)
+	{
+		check = n & divisor;
+		if (check == divisor)
+		{
+			flag = 1;
+			_putchar('1');
+		}
+		else if (flag == 1 || divisor == 1)
+		{
+			_putchar('0');
+		}
+		divisor >>= 1;
+	}
+}
---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: integer number
+ * @index: from 0 of the bit you want to get
+ * Returns: value of the bit at index or -1 if an error occure
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int
